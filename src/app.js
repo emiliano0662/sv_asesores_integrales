bb2abd0239870da2a997c5efea8d4fb116d448ff
Emiliano Carreño <emiliano0662@gmail.com>
2021-03-14 14:41:23 -0500

    completado

--- src/app.js
+++ src/app.js
@@ -13,5 +13,14 @@ var wow = new WOW({
 $('header').load("header.html");
 $('footer').load("footer.html");
 
+/*Script scroll*/
+$(document).on('click', '.navbar .navbar-nav .nav-item .nav-link', function (event) {
+    event.preventDefault();
+
+    var href = $(this).attr('href');
+
+    $('body,html').animate({ scrollTop: $(href).offset().top - 30 }, 2000, 'swing');
+});
+
 /*Se agregan las animaciones para toda la pagina que no cargan de menera recurrente*/
 wow.init();
